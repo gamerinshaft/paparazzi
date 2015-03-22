b45fe8ef5641fd7dd2883e18043330ecf0b7d597
j.taneichi <j.tane3ma@gmail.com>
2015-03-22 21:28:21 +0900

    setting timezone

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Paparazzi
         g.template_engine :haml
     end
     config.sass.preferred_syntax = :sass
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
